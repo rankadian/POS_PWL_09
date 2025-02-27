285b7e89c05439678c7548aed410c2565d4dd8b0
rankadian <dianthafafian@gmail.com>
2025-02-27 21:25:37 +0700

    DB Facade Implementation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// DB face cade implementation
+Route::get('/level', [LevelController::class, 'index']);
