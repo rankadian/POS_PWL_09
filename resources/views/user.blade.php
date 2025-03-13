97e6ed8061514b439ded8fac0fd855482275ae86
rankadian <dianthafafian@gmail.com>
2025-03-13 10:24:20 +0700

    Prakticum 2.6 Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,6 +7,7 @@
 
     <body>
         <h1>Data User</h1>
+        <a href="{{route('/user/tambah')}}">Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
@@ -18,14 +19,13 @@
 
             </tr>
             @foreach ($data as $d)
-                <tr>
-                    <td>{{ $data->user_id }}</td>
-                    <td>{{ $data->username }}</td>
-                    <td>{{ $data->nama }}</td>
-                    <td>{{ $data->level_id }}</td>
-                    {{-- <td>{{ $count }}</td> --}}
-                    <td><a href="{{route('user.ubah', $d->user_id)}}">ubah</a> | <a
-                            href="{{route('user.hapus', $d->user_id)}}">hapus</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                {{-- <td>{{ $count }}</td> --}}
+                <td><a href="{{route('/user/ubah', $d->user_id)}}">ubah</a> | <a
+                        href="{{route('/user/hapus', $d->user_id)}}">hapus</td>
                 </tr>
             @endforeach
         </table>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Name;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,9 +35,12 @@ Route::get('/kategori', [KategoriController::class, 'index']);
 // ELOQUENT ORM
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
 
 // Create, Read, Update, Delete (CRUD)
-Route::get('/user/tambah', [UserController::class, ''])->name('/user/tambah');
-Route::get('/user/ubah/{id}', [UserController::class, ''])->name('/user/ubah');
-Route::get('/user/hapus/{id}', [UserController::class, ''])->name('/user/hapus');
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
+Route::get('/user', [UserController::class, 'index'])->name('/user');
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
