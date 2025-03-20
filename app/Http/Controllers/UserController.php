e5d2d6618a2c8ed0bcab434e99bf4f9a8b1f09c7
rankadian <dianthafafian@gmail.com>
2025-03-20 13:31:59 +0700

    JS6 Practicum 3. Delete Data Ajax Modal (User Data)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Yajra\DataTables\Facades\DataTables;
 use App\Models\LevelModel;
 use App\Models\User;
@@ -243,5 +244,31 @@ class UserController extends Controller
             }
         }
         redirect('/');
+    }
+
+    public function confirm_ajax(string $id){
+        $user = UserModel::find($id);;
+
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+
+    public function delete_ajax(Request $request, $id){
+        // cek request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status'    => true,
+                    'message'   => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status'    => false,
+                    'message'   => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
     }
 }
