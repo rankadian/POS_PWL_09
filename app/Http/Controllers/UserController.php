6ff513ca005307d13d10603a44195f49e83a78f1
rankadian <dianthafafian@gmail.com>
2025-03-20 11:03:53 +0700

    JS6 Practicum 1. Add Data Ajax Modal (User Data)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use App\Models\LevelModel;
 use App\Models\User;
 use Dflydev\DotAccessData\Data;
 use Monolog\Level;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -148,5 +149,43 @@ class UserController extends Controller
         } catch (\Illuminate\Database\QueryException $e) {
             return redirect('/user')->with('error', 'Data user tidak bisa dihapus karena masih terdapat data terkait');
         }
+    }
+
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+            ->with('level', $level);
+    }
+
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id'  => 'required|integer',
+                'username'  => 'required|string|min:3|unique:m_user, username',
+                'nama'      => 'required|string|max:100',
+                'password'  => 'required|min:6'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status'    => false, //respone status, false: error/gagal, true: berhasil
+                    'messege'   => 'Validasi Gagal',
+                    'msgField'  => $validator->errors(), //pesan error validasi
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status'    => true,
+                'messege'   => 'Data user berhasil disimpan'
+            ]);
+        }
+        redirect('/');
     }
 }
