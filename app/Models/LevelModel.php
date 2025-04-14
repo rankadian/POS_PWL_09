6c9927c6bb6574827d5e637150773f969dd9184e
rankadian <dianthafafian@gmail.com>
2025-04-14 20:38:56 +0700

    update model level

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LevelModel extends Model
 {
-    use HasFactory;
     protected $table = "m_level";
     protected $primaryKey = "level_id";
     protected $fillable = [
         'level_kode',
         'level_nama',
     ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class);
+    }
 }
