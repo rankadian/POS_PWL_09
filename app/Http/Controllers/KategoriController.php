85afd660267b04d7bd3658920a05691c01e044a5
rankadian <dianthafafian@gmail.com>
2025-04-14 20:37:42 +0700

    make model kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,49 +2,13 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\LevelModel;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class KategoriController extends Controller
 {
-    public function insert()
-    {
-        DB::table('m_kategori')->insert(['kategori_kode' => 'SMP', 'kategori_nama' => 'Smartphone']);
-        return 'Data kategori ditambahakan';
-    }
-
-    public function update()
-    {
-        DB::table('m_kategori')->where('kategori_id', 1)->update(['kategori_nama' => 'Makanan Ringan']);
-        return 'Data kategori telah diupdate';
-    }
-
-    public function delete()
-    {
-        // DB::table('m_kategori')->where('kategori_id', 9)->delete();
-        $row = DB::delete('delete from m_kategori where Kategori_kode = ?', ['SMP']);
-        return 'Data kategori telah dihapus';
-    }
-
-
-    // Query Builder Implementation
-    public function index()
-    {
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
-    }
+        
 }
