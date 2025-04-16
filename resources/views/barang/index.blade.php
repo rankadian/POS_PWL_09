317df953a62bdc0509003df1ef3e7bdb0a941613
rankadian <dianthafafian@gmail.com>
2025-04-16 11:46:25 +0700

    foreach

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,7 +6,8 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -23,9 +24,9 @@
                         <div class="col-3">
                             <select class="form-control" id="kategori_id" name="kategori_id" required>
                                 <option value="">- Semua -</option>
-                                @foreach($kategori as $item)
+                                {{-- @foreach($kategori as $item)
                                     <option value="{{ $item->kategori_id }}">{{ $item->kategori_nama }}</option>
-                                @endforeach
+                                @endforeach --}}
                             </select>
                             <small class="form-text text-muted">Kategori Barang</small>
                         </div>
