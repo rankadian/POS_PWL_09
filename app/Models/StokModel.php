e780451e63b53bdc8a57f7e82d160b30eb6c13c3
rankadian <dianthafafian@gmail.com>
2025-04-16 16:47:28 +0700

    update relation

--- app/Models/StokModel.php
+++ app/Models/StokModel.php
@@ -14,19 +14,14 @@ class StokModel extends Model
         'user_id',
         'stok_tanggal',
         'stok_jumlah',
-        // 'suplier_id',
     ];
     public function barang()
     {
         return $this->belongsTo(BarangModel::class, 'barang_id', 'barang_id');
     }
+
     public function user()
     {
         return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
     }
-    // public function supplier()
-    // {
-    //     return $this->belongsTo(SupplierModel::class, 'supplier_id', 'supplier_id');
-    // }
-
 }
