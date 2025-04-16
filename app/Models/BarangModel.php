5366c1810e4e6452cd033df29a607be0ee05d424
rankadian <dianthafafian@gmail.com>
2025-04-16 16:25:18 +0700

    update relation model kategori and barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -2,22 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BarangModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
+
     protected $fillable = [
         'kategori_id',
         'barang_nama',
-        'barang_nama',
+        'barang_kode',
         'harga_beli',
-        'harga_jual',
+        'harga_jual'
     ];
-    public function kategori()
+
+    public function kategori(): BelongsTo
     {
-        return $this->hasOne(KategoriModel::class, 'kategori_id', 'kategori_id');
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
 }
--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -2,19 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class KategoriModel extends Model
 {
+    use HasFactory;
     protected $table = 'm_kategori';
     protected $primaryKey = 'kategori_id';
+
     protected $fillable = [
         'kategori_kode',
-        'kategori_nama',
+        'kategori_nama'
     ];
-    public function barang()
-    {
-        return $this->belongsTo(BarangModel::class);
-    }
 }
