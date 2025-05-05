74d49de5b6b1ced534fc41f701b32e38aebed0b5
rankadian <dianthafafian@gmail.com>
2025-05-05 10:33:49 +0700

    update detaoil button ajax

--- resources/views/detail_penjualan/edit_ajax.blade.php
+++ resources/views/detail_penjualan/edit_ajax.blade.php
@@ -24,92 +24,89 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Edit Detail Penjualan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria label="Close"><span
-                            aria-hidden="true">&times;</span></button>
+                    <button type="button" class="close" data-dismiss="modal" aria label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
-                    {{--  <div class="form-group">
-                        <label>Penjualan ID</label>
-                        <input value="{{ $detail_penjualan->penjualan_id }}" type="text" name="penjualan_id" id="penjualan_id" class="form-control" required>
-                        <small id="error-penjualan_id" class="error-text form-text text-danger"></small>
-                    </div>
-                    <div class="form-group">
-                        <label>Barang ID</label>
-                        <input value="{{ $detail_penjualan->barang_id }}" type="text" name="barang_id" id="barang_id" class="form-control" required>
-                        <small id="error-barang_id" class="error-text form-text text-danger"></small>
-                    </div>  --}}
-                    <div class="form-group">
-                        <label>Penjualan</label>
-                        <select name="penjualan_id" id="penjualan_id" class="form-control" required>
-                            <option value="">- Pilih Kode Penjualan -</option>
-                            @foreach ($penjualan as $item)
-                                <option value="{{ $item->penjualan_id }}"
-                                    {{ $detail_penjualan->penjualan_id == $item->penjualan_id ? 'selected' : '' }}>
-                                    {{ $item->penjualan_kode ?? 'ID: ' . $item->penjualan_id }}
-                                </option>
-                            @endforeach
-                        </select>
-                        <small id="error-penjualan_id" class="error-text form-text text-danger"></small>
-                    </div>
-                    <div class="form-group">
-                        <label>Barang</label>
-                        <select name="barang_id" id="barang_id" class="form-control" required>
-                            <option value="">- Pilih Barang -</option>
-                            @foreach ($barangs as $barang)
-                                <option value="{{ $barang->barang_id }}"
-                                    {{ $detail_penjualan->barang_id == $barang->barang_id ? 'selected' : '' }}>
-                                    {{ $barang->barang_kode }} - {{ $barang->nama_barang }}
-                                </option>
-                            @endforeach
-                        </select>
-                        <small id="error-barang_id" class="error-text form-text text-danger"></small>
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">User</label>
+                        <div class="col-11">
+                            <select class="form-control" id="user_id" name="user_id" required>
+                                <option value="">- Kode Penjualan -</option>
+                                @foreach($detail_penjualan as $p)
+                                    <option value="{{ $p->penjualan_id }}" @if(old('penjualan_id', $detail_penjualan->penjualan_id) == $p->penjualan_id) selected @endif>
+                                        {{ $p->penjualan_kode }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            @error('penjualan_id')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
                     </div>
 
-                    <div class="form-group">
-                        <label>Harga</label>
-                        <input value="{{ $detail_penjualan->harga }}" type="number" name="harga" id="harga"
-                            class="form-control" required>
-                        <small id="error-harga" class="error-text form-text text-danger"></small>
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">User</label>
+                        <div class="col-11">
+                            <select class="form-control" id="user_id" name="user_id" required>
+                                <option value="">- Barang -</option>
+                                @foreach($detail_penjualan as $p)
+                                    <option value="{{ $p->barang_id }}" @if(old('barang_id', $detail_penjualan->barang_id) == $p->barang_id) selected @endif>
+                                        {{ $p->barang_nama }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            @error('barang_id')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label>Jumlah</label>
-                        <input value="{{ $detail_penjualan->jumlah }}" type="number" name="jumlah" id="jumlah"
-                            class="form-control" required>
-                        <small id="error-jumlah" class="error-text form-text text-danger"></small>
+
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Harga</label>
+                        <div class="col-11">
+                            <input type="number" class="form-control" id="harga" name="harga"
+                                value="{{ old('harga', $detail_penjualan->harga) }}" required>
+                            @error('harga')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
+
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Jumlah</label>
+                        <div class="col-11">
+                            <input type="number" class="form-control" id="jumlah" name="jumlah"
+                                value="{{ old('jumlah', $detail_penjualan->jumlah) }}" required>
+                            @error('pembeli')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
                 </div>
             </div>
-        </div>
     </form>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $("#form-edit").validate({
                 rules: {
-                    penjualan_id: {
-                        required: true
-                    },
-                    barang_id: {
-                        required: true
-                    },
-                    harga: {
-                        required: true,
-                        min: 1
-                    },
-                    jumlah: {
-                        required: true,
-                        min: 1
-                    }
+                    penjualan_id: { required: true },
+                    barang_id: { required: true },
+                    harga: { required: true, min: 1 },
+                    jumlah: { required: true, min: 1 }
                 },
-                submitHandler: function(form) {
+                submitHandler: function (form) {
                     $.ajax({
                         url: form.action,
                         type: form.method,
                         data: $(form).serialize(),
-                        success: function(response) {
+                        success: function (response) {
                             if (response.status) {
                                 $('#myModal').modal('hide');
                                 Swal.fire({
@@ -117,10 +114,13 @@
                                     title: 'Berhasil',
                                     text: response.message
                                 });
-                                dataDetailPenjualan.ajax.reload();
+
+                                if (typeof dataDetailPenjualan !== 'undefined') {
+                                    dataDetailPenjualan.ajax.reload(null, false);
+                                }
                             } else {
                                 $('.error-text').text('');
-                                $.each(response.msgField, function(prefix, val) {
+                                $.each(response.msgField, function (prefix, val) {
                                     $('#error-' + prefix).text(val[0]);
                                 });
                                 Swal.fire({
@@ -134,14 +134,14 @@
                     return false;
                 },
                 errorElement: 'span',
-                errorPlacement: function(error, element) {
+                errorPlacement: function (error, element) {
                     error.addClass('invalid-feedback');
                     element.closest('.form-group').append(error);
                 },
-                highlight: function(element, errorClass, validClass) {
+                highlight: function (element, errorClass, validClass) {
                     $(element).addClass('is-invalid');
                 },
-                unhighlight: function(element, errorClass, validClass) {
+                unhighlight: function (element, errorClass, validClass) {
                     $(element).removeClass('is-invalid');
                 }
             });
